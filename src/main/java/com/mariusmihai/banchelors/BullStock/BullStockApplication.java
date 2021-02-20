c54d82d96b79b608c7e3535d3d59e8acf0fd8a4a
MariusMsw <mariusliviu70@yahoo.com>
2021-02-20 15:02:15 +0200

    first commit

---
+++ src/main/java/com/mariusmihai/banchelors/BullStock/BullStockApplication.java
@@ -0,0 +1,13 @@
+package com.mariusmihai.banchelors.BullStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariusmihai/banchelors/BullStock/BullStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariusmihai.banchelors.BullStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
