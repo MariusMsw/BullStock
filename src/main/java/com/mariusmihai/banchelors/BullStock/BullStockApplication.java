09cade23f33f06853c730b3cd43e9a38d85669ea
MariusMsw <mariusliviu70@yahoo.com>
2021-05-11 22:23:32 +0300

    add cors filter

--- src/main/java/com/mariusmihai/banchelors/BullStock/BullStockApplication.java
+++ src/main/java/com/mariusmihai/banchelors/BullStock/BullStockApplication.java
@@ -2,14 +2,34 @@ package com.mariusmihai.banchelors.BullStock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-@EnableJpaRepositories(basePackages="com.mariusmihai.banchelors.BullStock.repositories")
+@EnableJpaRepositories(basePackages = "com.mariusmihai.banchelors.BullStock.repositories")
 @SpringBootApplication
 public class BullStockApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BullStockApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BullStockApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilterRegistrationBean() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 
 }
